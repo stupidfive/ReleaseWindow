6840ab1b89450dfe2c5e73b43f85a392ff627654
GeorgeWu <wuzheng@zerozero.cn>
2016-09-20 23:15:03 +0800

    Initial Commit

---
+++ ReleaseWindow/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ReleaseWindow
+//
+//  Created by George Wu on 9/20/16.
+//  Copyright © 2016 George Wu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ReleaseWindow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReleaseWindow
+//
+//  Created by George Wu on 9/20/16.
+//  Copyright © 2016 George Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
